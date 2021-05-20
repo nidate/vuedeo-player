cce4efc6f95e713c5a6e8a3709a418c8b4f09288
NIIDATE Kunitaka <nidate@core-note.jp>
2021-05-20 21:07:54 +0900

    add applicaton definitions for macos

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      preload: 'src/preload.js'
+      preload: 'src/preload.js',
+      builderOptions: {
+        appId: 'jp.core-note.${name}',
+        fileAssociations: [
+          {
+            ext: ['mp4', 'mov', 'insv'],
+            name: 'Movie',
+            role: 'Viewer'
+          }
+        ],
+        mac: {
+          category: 'public.app-category.video',
+          target: 'dir',
+          extendInfo: {
+            CFBundlePackageType: 'APPL',
+            CFBundleDocumentTypes: [
+              {
+                CFBundleTypeRole: 'Viewer',
+                LSHandlerRank: 'Default',
+                LSItemContentTypes: [
+                  'public.mp4',
+                  'public.mpg',
+                  'public.mov',
+                  'public.insv'
+                ]
+              }
+            ]
+          }
+        }
+      }
     }
   }
 };
